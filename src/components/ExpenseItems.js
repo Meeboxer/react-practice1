a1637c02ac2abbad5b275eaefc41acfb47ceb567
Meebot <37608929+Meeboxer@users.noreply.github.com>
2022-02-21 09:08:01 +0800

    add 3 more expense items and connect expense.js to app.js

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,15 +1,15 @@
-import './ExpenseItems.css';
+import "./ExpenseItems.css";
 
-function ExpenseItem() {
-    const ExpenseDate = new Date(2022,1,18);
-    const ExpenseItem = "Dinner";
-    const ExpensePrice = 200;
+function ExpenseItem(props) {
+  const ExpenseDate = new Date(2022, 1, 18);
+  const ExpenseItem = "Dinner";
+  const ExpensePrice = 200;
   return (
-    <div className='expense-item'>
-      <div>{ExpenseDate.toDateString()}</div>
-      <div className='expense-item__description'>
-        <h2>{ExpenseItem}</h2>  
-        <div className='expense-item__price'>${ExpensePrice}</div>
+    <div className="expense-item">
+      <div>{props.date.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
